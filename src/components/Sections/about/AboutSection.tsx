7770218dd2f1a390991b26f4fe9501a1bbc739df
AeshuChauhan <aeshuchauhan@gmail.com>
2024-03-04 22:15:33 +0530

    add resume

--- src/components/Sections/about/AboutSection.tsx
+++ src/components/Sections/about/AboutSection.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { TextGenerate } from './AboutMe'
 import { SkillTooltip } from './SkillsToolTip'
 import skillData from '@/utils/skillData'
+import { FaDownload } from 'react-icons/fa';
 export default function AboutSection() {
     const { programmingLanguage, frontEnd, backEnd, db, otherTool } = skillData();
     return (
@@ -13,8 +14,20 @@ export default function AboutSection() {
             <div className=''>
                 <TextGenerate />
                 <div className="my-5">
-                    <p className='text-xl font-bold text-yellow-500'>My Skills</p>
-                    <div className='mt-1 w-12 border-b-2 rounded-lg border-cyan-600 shadow-md shadow-yellow-500/40'></div>
+                    <div className='flex justify-between'>
+                        <div>
+                            <p className='text-xl font-bold text-yellow-500'>My Skills</p>
+                            <div className='mt-1 w-12 border-b-2 rounded-lg border-cyan-600 shadow-md shadow-yellow-500/40'></div>
+                        </div>
+                        <div>
+                            <button className='flex gap-3 me-1'>
+                                <span className='text-lg font-bold text-yellow-500 hidden sm:block'>Resume</span>
+                                <FaDownload size={20} className='text-cyan-500' />
+                            </button>
+                            <div className='mt-1 w-12 border-b-2 rounded-lg hidden sm:block border-cyan-600 shadow-md shadow-yellow-500/40'></div>
+
+                        </div>
+                    </div>
                     <div className="border-2 rounded my-3">
                         <div className='flex flex-col justify-center w-full px-5 py-5'>
                             <p className="line-clamp-2 text-sm mb-2 leading-snug ">{'Programming Languages:'}</p>
--- src/components/Sections/about/SkillsToolTip.tsx
+++ src/components/Sections/about/SkillsToolTip.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export function SkillTooltip({ skills }: any) {
     return (
-        <div className="flex flex-row items-start justify-start mb-5 w-full gap-5">
+        <div className="flex flex-row flex-wrap items-start justify-start mb-5 w-full gap-5">
             <AnimatedTooltip items={skills} />
         </div>
     );
